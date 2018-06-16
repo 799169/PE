af00c0b3100ea182cc4688635fdfef6af8e55d48
799169 <38424609+799169@users.noreply.github.com>
2018-06-17 04:55:47 +0530

    Create Problem28.cpp

---
+++ Problems 1 to 100/Problems 21 to 30/Problem28.cpp
@@ -0,0 +1,107 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <map>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+
+using namespace std;
+using namespace chrono;
+
+
+namespace PrimeUtils {
+
+	const int MAXN = 1e3;
+
+	const int MAXS = MAXN / 3;
+	bool sieve[MAXS];
+
+	const int MAXP = MAXN / 5;
+	int primes[MAXP];
+	int primesCount;
+
+	inline int getNumber(int i) {
+		return (i % 2 == 0) ? (6 * (i / 2) + 5) : (6 * (i / 2) + 7);
+	}
+
+	inline int getIndex(int n) {
+		return ((n - 1) % 6 == 0) ? ((n - 4) / 3) : ((n - 5) / 3);
+	}
+
+	void generatePrimes() {
+		int j, k;
+		for (int i = 0; i < MAXS; ++i) {
+			sieve[i] = true;
+		}
+		for (int i = 0; i < MAXS; ++i) {
+			if (sieve[i]) {
+				j = getNumber(i);
+				if (j * j > MAXN) {
+					break;
+				}
+				if (j % 6 == 1) {
+					for (k = j * j; k <= MAXN; k += 6 * j) {
+						sieve[getIndex(k)] = false;
+						sieve[getIndex(k + 4 * j)] = false;
+					}
+				} else if (j % 6 == 5) {
+					for (k = j * j; k <= MAXN; k += 6 * j) {
+						sieve[getIndex(k)] = false;
+						sieve[getIndex(k + 2 * j)] = false;
+					}
+				}
+			}
+		}
+		primes[0] = 2;
+		primes[1] = 3;
+		primesCount = 2;
+		for (int i = 0; i < MAXS; ++i) {
+			if (sieve[i]) {
+				primes[primesCount] = getNumber(i);
+				++primesCount;
+			}
+		}
+		while (primes[primesCount - 1] > MAXN) {
+			--primesCount;
+		}
+	}
+
+}
+
+struct Problem28 {
+
+	auto solve() {
+		const int N = 1001;
+		int m = (N - 1) / 2;
+		return 2LL * m * (m * (8 * m + 15) + 13) / 3 + 1;
+	}
+
+} solver;
+
+
+int main() {
+	freopen("output.txt", "w+", stdout);
+	auto start = high_resolution_clock::now();
+	auto ans = solver.solve();
+	auto end = high_resolution_clock::now();
+	duration <double> diff = end - start;
+	cout << setw(12) << " Answer: " << setw(10) << ans;
+	cout << "\n Time taken: " << setw(9) << fixed;
+	cout << setprecision(5) << diff.count() << "s\n\n";
+	return 0;
+}
+
+/***********************
+    Answer:  669171001
+ Time taken:   0.00000s
+***********************/
