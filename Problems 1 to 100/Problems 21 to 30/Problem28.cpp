5d4bfdf1feb3a087b76ac9ee269296dfd0c6d58c
799169 <38424609+799169@users.noreply.github.com>
2018-06-17 04:56:47 +0530

    Update Problem28.cpp

--- Problems 1 to 100/Problems 21 to 30/Problem28.cpp
+++ Problems 1 to 100/Problems 21 to 30/Problem28.cpp
@@ -19,65 +19,6 @@ using namespace std;
 using namespace chrono;
 
 
-namespace PrimeUtils {
-
-	const int MAXN = 1e3;
-
-	const int MAXS = MAXN / 3;
-	bool sieve[MAXS];
-
-	const int MAXP = MAXN / 5;
-	int primes[MAXP];
-	int primesCount;
-
-	inline int getNumber(int i) {
-		return (i % 2 == 0) ? (6 * (i / 2) + 5) : (6 * (i / 2) + 7);
-	}
-
-	inline int getIndex(int n) {
-		return ((n - 1) % 6 == 0) ? ((n - 4) / 3) : ((n - 5) / 3);
-	}
-
-	void generatePrimes() {
-		int j, k;
-		for (int i = 0; i < MAXS; ++i) {
-			sieve[i] = true;
-		}
-		for (int i = 0; i < MAXS; ++i) {
-			if (sieve[i]) {
-				j = getNumber(i);
-				if (j * j > MAXN) {
-					break;
-				}
-				if (j % 6 == 1) {
-					for (k = j * j; k <= MAXN; k += 6 * j) {
-						sieve[getIndex(k)] = false;
-						sieve[getIndex(k + 4 * j)] = false;
-					}
-				} else if (j % 6 == 5) {
-					for (k = j * j; k <= MAXN; k += 6 * j) {
-						sieve[getIndex(k)] = false;
-						sieve[getIndex(k + 2 * j)] = false;
-					}
-				}
-			}
-		}
-		primes[0] = 2;
-		primes[1] = 3;
-		primesCount = 2;
-		for (int i = 0; i < MAXS; ++i) {
-			if (sieve[i]) {
-				primes[primesCount] = getNumber(i);
-				++primesCount;
-			}
-		}
-		while (primes[primesCount - 1] > MAXN) {
-			--primesCount;
-		}
-	}
-
-}
-
 struct Problem28 {
 
 	auto solve() {
