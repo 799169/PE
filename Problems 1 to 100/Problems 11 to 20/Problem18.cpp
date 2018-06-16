fc8cd945ca2e12ea3f9fa8cb10c3fd5ce8d530b3
799169 <38424609+799169@users.noreply.github.com>
2018-06-16 06:16:26 +0530

    Create Problem18.cpp

---
+++ Problems 1 to 100/Problems 11 to 20/Problem18.cpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <map>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+
+using namespace std;
+using namespace chrono;
+
+
+struct Problem18 {
+
+	string s = "\
+		75\
+		95 64\
+		17 47 82\
+		18 35 87 10\
+		20 04 82 47 65\
+		19 01 23 75 03 34\
+		88 02 77 73 07 63 67\
+		99 65 04 28 06 16 70 92\
+		41 41 26 56 83 40 80 70 33\
+		41 48 72 33 47 32 37 16 94 29\
+		53 71 44 65 25 43 91 52 97 51 14\
+		70 11 33 28 77 73 17 78 39 68 17 57\
+		91 71 52 38 17 14 91 43 58 50 27 29 48\
+		63 66 04 68 89 53 67 30 73 16 69 87 40 31\
+		04 62 98 27 23 09 70 98 73 93 38 53 60 04 23\
+	";
+
+	auto solve() {
+		stringstream ss(s);
+		const int N = 15;
+		int a[N][N], s[N][N], mx = 0;
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j <= i; ++j) {
+				ss >> a[i][j];
+			}
+		}
+		for (int i = 1; i < N; ++i) {
+			for (int j = 1; j < i; ++j) {
+				a[i][j] += max(a[i - 1][j - 1], a[i - 1][j]);
+				mx = max(a[i][j], mx);
+			}
+			a[i][0] += a[i - 1][0];
+			a[i][i] += a[i - 1][i - 1];
+			mx = max(a[i][0], mx);
+			mx = max(a[i][i], mx);
+		}
+		return mx;
+	}
+
+} solver;
+
+
+int main() {
+	freopen("output.txt", "w+", stdout);
+	auto start = high_resolution_clock::now();
+	auto ans = solver.solve();
+	auto end = high_resolution_clock::now();
+	duration <double> diff = end - start;
+	cout << setw(12) << " Answer: " << setw(10) << ans;
+	cout << "\n Time taken: " << setw(9) << fixed;
+	cout << setprecision(5) << diff.count() << "s\n\n";
+	return 0;
+}
+
+/***********************
+    Answer:       1074
+ Time taken:   0.00000s
+***********************/
