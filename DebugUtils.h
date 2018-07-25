cb1e99c5233d55e9c9c9559924eaffa061dcdedc
799169 <38424609+799169@users.noreply.github.com>
2018-07-25 19:38:46 +0530

    Update DebugUtils.h

--- DebugUtils.h
+++ DebugUtils.h
@@ -1,76 +1,195 @@
-#include <algorithm>
-#include <chrono>
-#include <cmath>
-#include <cstdlib>
-#include <fstream>
-#include <functional>
-#include <iostream>
-#include <iomanip>
-#include <iterator>
-#include <map>
-#include <set>
-#include <sstream>
-#include <string>
-#include <tuple>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-typedef istream_iterator <string> _iis;
-typedef stringstream _tss;
+typedef stringstream tss;
+typedef istream_iterator <string> iis;
 
-#define tr(x...) { string _s = #x; replace(begin(_s), end(_s), ',', ' '); _tss _ss(_s); _iis _it(_ss); trx(_it, x); }
+#define all(x) begin(x), end(x)
 
-void trx(_iis it) { cout << endl; }
+#define tr(x...) { string _s = #x; replace(all(_s), ',', ' '); tss _ss(_s); iis _it(_ss); trx(_it, x); }
 
-template <class T, class... U> void trx(_iis it, T a, U... u) {
+void trx(iis it) { cout << endl; }
+
+template <class T, class... U>
+void trx(iis it, T a, U... u) {
 	cout << *it << '=' << a << ' ';
 	trx(++it, u...);
 }
 
 namespace DebugUtils  {
 
-	void print() {
-		cout << ' ';
+	template <class T>
+	void print(const T t) {
+		cout << t;
+	}
+
+	template <class T, class... U>
+	void print(const T t, const U... u) {
+		print(t);
+		print(' ');
+		print(u...);
 	}
 
 	void println() {
 		cout << '\n';
 	}
 
-	template <class T> void print(const vector <T> &v) {
-		for (int i = 0; i < v.size(); ++i) {
-			cout << v[i] << ' ';
-		}
-		cout << '\n';
-	}
-
-	template <class T> void print(const T &t) {
-		cout << t << ' ';
-	}
-
-	template <class T> void println(const T &t) {
-		cout << t << '\n';
-	}
-
-	template <class T> void print(T *a, int n) {
-		for (int i = 0; i < n; ++i) {
-			cout << a[i] << ' ';
-		}
-		cout << endl;
-	}
-
-	template <class T, class... U> void print(const T &t, const U... u) {
+	template <class T>
+	void println(const T t) {
 		print(t);
-		print(u...);
+		print('\n');
 	}
 
-	template <class T, class... U> void println(const T &t, const U... u) {
+	template <class T, class... U>
+	void println(const T t, const U... u) {
 		print(t);
+		print(' ');
 		println(u...);
 	}
 
-}
+	template <class T, class U>
+	void print(const pair <T, U> p) {
+		print('{');
+		print(p.first);
+		print(",");
+		print(p.second);
+		print('}');
+	}
+
+	template <class T, class U>
+	void println(const pair <T, U> p) {
+		print(p);
+		println();
+	}
+
+	template <class T>
+	void print(vector <T> v, int n) {
+		for (int i = 0; i < n; ++i) {
+			if (i != 0) {
+				print(' ');
+			}
+			print(v[i]);
+		}
+	}
+
+	template <class T>
+	void print(vector <T> v) {
+		print(v, v.size());
+	}
+
+
+	template <class T>
+	void println(vector <T> v, int n) {
+		print(v, n);
+		println();
+	}
+
+	template <class T>
+	void println(vector <T> v) {
+		print(v, v.size());
+		println();
+	}
+
+
+	template <class T>
+	void print(set <T> v, int n) {
+		auto e = begin(v);
+		for (int i = 0; i < n; ++i) {
+			if (i != 0) {
+				print(' ');
+			}
+			print(*e);
+			++e;
+		}
+	}
+
+	template <class T>
+	void print(set <T> v) {
+		print(v, v.size());
+	}
+
+
+	template <class T>
+	void println(set <T> v, int n) {
+		print(v, n);
+		println();
+	}
+
+	template <class T>
+	void println(set <T> v) {
+		print(v, v.size());
+		println();
+	}
+
+	template <class T, class U>
+	void print(map <T, U> v, int n) {
+		auto e = begin(v);
+		for (int i = 0; i < n; ++i) {
+			if (i != 0) {
+				print(' ');
+			}
+			print(e -> first);
+			print(':');
+			print(e.second);
+			++e;
+		}
+	}
+
+	template <class T, class U>
+	void print(map <T, U> v) {
+		print(v, v.size());
+	}
+
+
+	template <class T, class U>
+	void println(map <T, U> v, int n) {
+		print(v, n);
+		println();
+	}
+
+	template <class T, class U>
+	void println(map <T, U> v) {
+		print(v, v.size());
+		println();
+	}
+
+	template <class T>
+	void print(vector <T> *a, int n) {
+		for (int i = 0; i < n; ++i) {
+			println(a[i]);
+		}
+	}
+
+	template <class T>
+	void println(vector <T> *a, int n) {
+		print(a, n);
+		println();
+	}
+
+	template <class T>
+	void print(T *a, int n) {
+		for (int i = 0; i < n; ++i) {
+			if (i != 0) {
+				print(' ');
+			}
+			print(a[i]);
+		}
+	}
+
+	template <class T>
+	void println(T *a, int n) {
+		print(a, n);
+		println();
+	}
+
+};
 
 using namespace DebugUtils;
 
+
+int main() {
+	freopen("output.txt", "w+", stdout);
+
+	return 0;
+}
