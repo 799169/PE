fbb4f8a5fe57e3b204a8638563caac46bf085fc4
799169 <38424609+799169@users.noreply.github.com>
2020-03-16 00:36:36 +0530

    Create Problem69.cpp

---
+++ Problems 1 to 100/Problems 61 to 70/Problem69.cpp
@@ -0,0 +1,131 @@
+#include "DebugUtils.cpp"
+
+
+const int MAXN = 1e6;
+const int SQRTMAXN = sqrt(MAXN);
+const int MAXS = MAXN / 3;	// number of candidates to be checked; all are of the form 6k + 1 or 6k - 1
+const int MAXP = MAXN / 10;	// max number of primes expected <= MAXN
+bitset<MAXS> sieve;
+// bool sieve[MAXS];
+int primes[MAXP] = {2, 3}, numOfPrimes = 2;
+
+// MAXN=1e5 numOfPrimes=   9592 (0.09592)	< 10% numbers are prime
+// MAXN=1e6 numOfPrimes=  78498 (0.078498)	<  8% numbers are prime
+// MAXN=1e7 numOfPrimes= 664579 (0.0664579) 0.05seconds
+// MAXN=1e8 numOfPrimes=5761455 (0.0576146) 0.5seconds with bitset 0.8seconds with bool[]
+
+
+
+// if i is even (0, 2, 4): then the index i in sieve holds 3 * i + 5
+// if i is odd  (1, 3, 5): then the index i in sieve holds 3 * i + 4
+inline int getNumber(int i) {
+	return 3 * i + 5 - (i & 1);
+}
+
+// if n == 1 (mod 6) then n is at index (n - 4) / 3
+// if n == 5 (mod 6) then n is at index (n - 5) / 3
+inline int getIndex(int n) {
+	return (n % 6 == 1) ? (n - 4) / 3 : (n - 5) / 3;
+}
+
+// all primes <= MAXN
+void generatePrimes() {
+	// fill(sieve, sieve + MAXS, true);
+	// fill_n(sieve, MAXS, true);
+	sieve.set();
+	for (int i = 0; i < MAXS; ++i) {
+		// if i odd ,  j == 1 (mod 6),  j * j == 1 (mod 6),  j * j + 4 * j == 5 (mod 6)
+		// if i even,  j == 5 (mod 6),  j * j == 1 (mod 6),  j * j + 2 * j == 5 (mod 6)
+		// tr(i, getNumber(i), sieve[i])
+		if (sieve[i]) {
+			int j = getNumber(i);
+			if (j > SQRTMAXN) {
+				break;
+			}
+			for (int k = j * j; k <= MAXN; k += 6 * j) {
+				sieve[(k - 4) / 3] = false;
+			}
+			for (int k = j * j + 2 * j * ((i & 1) + 1); k <= MAXN; k += 6 * j) {
+				sieve[(k - 5) / 3] = false;
+			}
+		}
+	}
+	for (int i = 0; i < MAXS; ++i) {
+		if (sieve[i]) {
+			primes[numOfPrimes] = getNumber(i);
+			++numOfPrimes;
+		}
+	}
+	while (primes[numOfPrimes - 1] > MAXN) {
+		--numOfPrimes;
+	}
+}
+
+
+
+const int MAXF = 8;	// max number of distinct prime factors we expect the number to have
+// 8 is enough for integers upto 9e6
+int primeFactors[MAXF];
+int powers[MAXF], numOfPrimeFactors;
+
+const int N = 1e6;
+
+
+void solve() {
+	generatePrimes();
+	LD best = 3, curr;
+	int ans = 6, n, m;
+	for (int nn = 1; nn <= 1e6; ++nn) {
+		n = nn;
+		m = sqrt(n) + 1;
+		curr = 1;
+		for (int i = 0; i < numOfPrimes; ++i) {
+			int p = primes[i];
+			if (p > m) {
+				if (n > 1) {
+					curr *= (p - 1.0L) / p;
+				}
+				break;
+			}
+			if (n % p == 0) {
+				n /= p;
+				while (n % p == 0) {
+					n /= p;
+				}
+				curr *= (p - 1.0L) / p;
+			}
+		}
+		if (curr < best) {
+			best = curr;
+			ans = nn;
+		}
+	}
+	tr(ans);
+	return;
+}
+
+int main() {
+	#ifdef LOCAL_DEFINE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w+", stdout);
+	#else
+		ios_base::sync_with_stdio(false);
+		cin.tie(nullptr);
+		cout.tie(nullptr);
+	#endif
+	auto t1 = chrono::high_resolution_clock::now();
+	solve();
+	auto t2 = chrono::high_resolution_clock::now();
+	chrono::duration<double> t = t2 - t1;
+	#ifdef LOCAL_DEFINE
+		cout << "\nTime taken: " << fixed;
+		cout << setprecision(5) << t.count() << "s\n\n";
+	#endif
+	return 0;
+}
+
+
+// ans=510510 
+
+// Time taken: 0.53147s
+
